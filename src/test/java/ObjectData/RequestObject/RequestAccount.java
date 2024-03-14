8ad7dbf54aca8c23837f11231af116a6a6a5a163
Hunor Csegzi <Hunor.Csegzi@endava.com>
2024-03-14 11:40:34 +0200

    token generated/ test3

--- src/test/java/ObjectData/RequestObject/RequestAccount.java
+++ src/test/java/ObjectData/RequestObject/RequestAccount.java
@@ -1,4 +1,4 @@
-package ObjectData;
+package ObjectData.RequestObject;
 
 
 import ObjectData.RequestPreparation;
--- src/test/java/Tests/CreateAccountTest.java
+++ src/test/java/Tests/CreateAccountTest.java
@@ -1,7 +1,10 @@
+
 package Tests;
 
-
-import ObjectData.RequestAccount;
+import ObjectData.RequestObject.RequestAccount;
+import ObjectData.ResponseObject.ResponseAccountGetSuccess;
+import ObjectData.ResponseObject.ResponseAccountSuccess;
+import ObjectData.ResponseObject.ResponseTokenSuccess;
 import PropertyUtility.PropertyUtility;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -10,35 +13,89 @@ import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
-
-
 public class CreateAccountTest {
+
+    public String userId;
+    public String token;
+    public RequestAccount requestAccountBody;
 
     @Test
     public void testMethod(){
-        //client definition
+        System.out.println("Step 1: create new account");
+        createAccount();
+        System.out.println();
+
+        System.out.println("Step 2: generate new token");
+        generateToken();
+        System.out.println();
+
+        System.out.println("Step 3: get new account");
+        getSpecificAccount();
+    }
+
+    public void createAccount(){
+        //define the client
         RequestSpecification requestSpecification = RestAssured.given();
         requestSpecification.baseUri("https://demoqa.com/");
         requestSpecification.contentType("application/json");
 
-        //definim request-ul
+        //define the request
         PropertyUtility propertyUtility = new PropertyUtility("RequestData/createAccountData");
-        RequestAccount requestAccountBody = new RequestAccount(propertyUtility.getAllData());
+        requestAccountBody = new RequestAccount(propertyUtility.getAllData());
         requestSpecification.body(requestAccountBody);
 
-        requestSpecification.body(requestAccountBody);
-
-        //response creation
+        //interaction with the response
         Response response = requestSpecification.post("Account/v1/User");
         System.out.println(response.getStatusCode());
+        Assert.assertEquals(response.getStatusCode(),201);
         System.out.println(response.getStatusLine());
-        ResponseBody responseBody = response.getBody();
-        System.out.println(responseBody.asPrettyString());
 
-
+        //validate response
+        ResponseAccountSuccess responseAccountBody = response.body().as(ResponseAccountSuccess.class);
+        System.out.println(responseAccountBody.getUserID());
+        userId = responseAccountBody.getUserID();
+        Assert.assertEquals(responseAccountBody.getUsername(),requestAccountBody.getUserName());
     }
 
+    public void generateToken(){
+        //define the client
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://demoqa.com/");
+        requestSpecification.contentType("application/json");
 
+        requestSpecification.body(requestAccountBody);
 
+        //interaction with the response
+        Response response = requestSpecification.post("Account/v1/GenerateToken");
+        System.out.println(response.getStatusCode());
+        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println(response.getStatusLine());
+
+        //validate response
+        ResponseTokenSuccess responseTokenSuccess = response.body().as(ResponseTokenSuccess.class);
+        System.out.println(responseTokenSuccess.getToken());
+        token = responseTokenSuccess.getToken();
+
+        Assert.assertEquals(responseTokenSuccess.getStatus(),"Success");
+        Assert.assertEquals(responseTokenSuccess.getResult(),"User authorized successfully.");
+    }
+
+    public void getSpecificAccount(){
+
+        //define the client
+        RequestSpecification requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://demoqa.com/");
+        requestSpecification.contentType("application/json");
+        requestSpecification.header("Authorization", "Bearer " + token);
+
+        //interaction with the response
+        Response response = requestSpecification.get("Account/v1/User/" + userId);
+        System.out.println(response.getStatusCode());
+        Assert.assertEquals(response.getStatusCode(),200);
+        System.out.println(response.getStatusLine());
+
+        //validate response
+        ResponseAccountGetSuccess responseAccountGetSuccess = response.body().as(ResponseAccountGetSuccess.class);
+        Assert.assertEquals(responseAccountGetSuccess.getUsername(),requestAccountBody.getUserName());
+    }
 }
